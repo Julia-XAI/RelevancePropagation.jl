b5ea495585cd1aa7879b316fcbb54cccf95ac859
Adrian Hill <adrian.hill@mailbox.org>
2023-12-07 18:39:31 +0100

    Initial functionality (#1)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,52 @@
-using LRP
+using LoopVectorization
+using LayerwiseRelevancePropagation
+using Flux
 using Test
+using ReferenceTests
 using Aqua
+using Random
 
-@testset "LRP.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(LRP)
+pseudorand(dims...) = rand(MersenneTwister(123), Float32, dims...)
+
+@testset "LayerwiseRelevancePropagation.jl" begin
+    @testset "Aqua.jl" begin
+        @info "Running Aqua.jl's auto quality assurance tests. These might print warnings from dependencies."
+        Aqua.test_all(LayerwiseRelevancePropagation; ambiguities=false)
     end
-    # Write your tests here.
+    @testset "Utilities" begin
+        @info "Testing utilities..."
+        include("test_utils.jl")
+    end
+    @testset "Flux utilities" begin
+        @info "Testing chainmap..."
+        include("test_chainmap.jl")
+    end
+    @testset "Canonize" begin
+        @info "Testing model canonization..."
+        include("test_canonize.jl")
+    end
+    @testset "Composites" begin
+        @info "Testing LRP composites..."
+        include("test_composite.jl")
+    end
+    @testset "Model checks" begin
+        @info "Testing LRP model checks..."
+        include("test_checks.jl")
+    end
+    @testset "LRP rules" begin
+        @info "Testing LRP rules..."
+        include("test_rules.jl")
+    end
+    @testset "CRP" begin
+        @info "Testing CRP..."
+        include("test_crp.jl")
+    end
+    @testset "CNN" begin
+        @info "Testing analyzers on CNN..."
+        include("test_cnn.jl")
+    end
+    @testset "Batches" begin
+        @info "Testing analyzers on batches..."
+        include("test_batches.jl")
+    end
 end
---
+++ test/test_cnn.jl
@@ -0,0 +1,106 @@
+using LoopVectorization
+using LayerwiseRelevancePropagation
+using Flux
+using JLD2
+
+const LRP_ANALYZERS = Dict(
+    "LRPZero"                   => LRP,
+    "LRPZero_COC"               => m -> LRP(m; flatten=false), # chain of chains
+    "LRPEpsilonAlpha2Beta1Flat" => m -> LRP(m, EpsilonAlpha2Beta1Flat()),
+)
+
+input_size = (32, 32, 3, 1)
+input = pseudorand(input_size)
+
+init(dims...) = Flux.glorot_uniform(MersenneTwister(123), dims...)
+
+model = Chain(
+    Chain(
+        Conv((3, 3), 3 => 8, relu; pad=1, init=init),
+        Conv((3, 3), 8 => 8, relu; pad=1, init=init),
+        MaxPool((2, 2)),
+        Conv((3, 3), 8 => 16, relu; pad=1, init=init),
+        Conv((3, 3), 16 => 16, relu; pad=1, init=init),
+        MaxPool((2, 2)),
+    ),
+    Chain(
+        Flux.flatten,
+        Dense(1024 => 512, relu; init=init),
+        Dropout(0.5),
+        Dense(512 => 100, relu; init=init),
+    ),
+)
+Flux.testmode!(model, true)
+
+function test_cnn(name, method)
+    @testset "$name" begin
+        @testset "Max activation" begin
+            # Reference test explanation
+            analyzer = method(model)
+            println("Timing $name...")
+            print("cold:")
+            @time expl = analyze(input, analyzer)
+
+            @test size(expl.val) == size(input)
+            @test_reference "references/cnn/$(name)_max.jld2" Dict("expl" => expl.val) by =
+                (r, a) -> isapprox(r["expl"], a["expl"]; rtol=0.05)
+        end
+        @testset "Neuron selection" begin
+            analyzer = method(model)
+            print("warm:")
+            @time expl = analyze(input, analyzer, 1)
+
+            @test size(expl.val) == size(input)
+            @test_reference "references/cnn/$(name)_ns1.jld2" Dict("expl" => expl.val) by =
+                (r, a) -> isapprox(r["expl"], a["expl"]; rtol=0.05)
+        end
+    end
+end
+
+# Run analyzers
+@testset "LRP analyzers" begin
+    for (name, method) in LRP_ANALYZERS
+        test_cnn(name, method)
+    end
+end
+
+@testset "CRP" begin
+    composite = EpsilonPlus()
+    layer_index = 5 # last Conv layer
+    n_concepts = 2
+    concepts = TopNConcepts(n_concepts)
+    analyzer = CRP(LRP(model, composite), layer_index, concepts)
+
+    @testset "Max activation" begin
+        println("Timing CRP...")
+        print("cold:")
+        @time expl = analyze(input, analyzer)
+
+        @test size(expl.val) == size(input) .* (1, 1, 1, n_concepts)
+        @test_reference "references/cnn/CRP_max.jld2" Dict("expl" => expl.val) by =
+            (r, a) -> isapprox(r["expl"], a["expl"]; rtol=0.05)
+    end
+    @testset "Neuron selection" begin
+        print("warm:")
+        @time expl = analyze(input, analyzer, 1)
+
+        @test size(expl.val) == size(input) .* (1, 1, 1, n_concepts)
+        @test_reference "references/cnn/CRP_ns1.jld2" Dict("expl" => expl.val) by =
+            (r, a) -> isapprox(r["expl"], a["expl"]; rtol=0.05)
+    end
+end
+
+# Layerwise relevances in LRP methods
+@testset "Layerwise relevances" begin
+    analyzer1 = LRP(model)
+    analyzer2 = LRP(model; flatten=false)
+    e1 = analyze(input, analyzer1; layerwise_relevances=true)
+    e2 = analyze(input, analyzer2; layerwise_relevances=true)
+    lwr1 = e1.extras.layerwise_relevances
+    lwr2 = e2.extras.layerwise_relevances
+
+    @test length(lwr1) == 11 # 10 layers in flattened VGG11
+    @test length(lwr2) == 3 # 2 chains in unflattened VGG11
+    @test lwr1[1] ≈ lwr2[1]
+    @test lwr1[end] ≈ lwr2[end]
+end
