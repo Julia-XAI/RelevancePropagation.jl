7e26dad012e1e4ae91632856a0c115a0cb618bed
Adrian Hill <adrian.hill@mailbox.org>
2024-01-15 19:11:36 +0100

    Add LRP documentation from ExplainableAI.jl (#4)

---
+++ docs/make.jl
@@ -0,0 +1,51 @@
+using Documenter
+using Literate
+using RelevancePropagation
+
+LITERATE_DIR = joinpath(@__DIR__, "src/literate")
+OUT_DIR = joinpath(@__DIR__, "src/generated")
+
+# Use Literate.jl to generate docs and notebooks of examples
+function convert_literate(dir_in, dir_out)
+    for p in readdir(dir_in)
+        path = joinpath(dir_in, p)
+
+        if isdir(path)
+            convert_literate(path, joinpath(dir_out, p))
+        else # isfile
+            Literate.markdown(path, dir_out; documenter=true) # Markdown for Documenter.jl
+            Literate.notebook(path, dir_out) # .ipynb notebook
+            Literate.script(path, dir_out) # .jl script
+        end
+    end
+end
+convert_literate(LITERATE_DIR, OUT_DIR)
+
+makedocs(;
+    modules=[RelevancePropagation],
+    authors="Adrian Hill",
+    sitename="RelevancePropagation.jl",
+    format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true", assets=String[]),
+    #! format: off
+    pages=[
+        "Home"            => "index.md",
+        "Basic usage"     => "generated/basics.md",
+        "Advanced usage"  => Any[
+            "Assigning rules to layers"     => "generated/composites.md",
+            "Supporting new layer types"    => "generated/custom_layer.md",
+            "Custom LRP rules"              => "generated/custom_rules.md",
+            "Concept Relevance Propagation" => "generated/crp.md",
+            "Developer documentation"       => "developer.md",
+        ],
+        "API reference"   => "api.md",
+    ],
+    #! format: on
+    linkcheck=true,
+    linkcheck_ignore=[
+        r"https://link.springer.com/chapter/10.1007/978-3-030-28954-6_10",
+        r"https://www.nature.com/articles/s42256-023-00711-8",
+    ],
+    checkdocs=:exports, # only check docstrings in API reference if they are exported
+)
+
+deploydocs(; repo="github.com/Julia-XAI/RelevancePropagation.jl")
---
+++ docs/src/index.md
@@ -0,0 +1,42 @@
+```@meta
+CurrentModule = RelevancePropagation
+```
+
+# RelevancePropagation.jl
+
+Julia implementation of [Layerwise Relevance Propagation](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0130140) (LRP) 
+and [Concept Relevance Propagation](https://www.nature.com/articles/s42256-023-00711-8) (CRP) 
+for use with [Flux.jl](https://fluxml.ai) models.
+
+!!! note
+    This package is part the [Julia-XAI ecosystem](https://github.com/Julia-XAI) and compatible with
+    [ExplainableAI.jl](https://github.com/Julia-XAI/ExplainableAI.jl).
+
+    For an introduction to the Julia-XAI ecosystem, refer to the 
+    [*Getting started* guide from ExplainableAI.jl](https://julia-xai.github.io/ExplainableAI.jl/stable/generated/example/).
+
+
+
+## Installation 
+To install this package and its dependencies, open the Julia REPL and run 
+```julia-repl
+julia> ]add RelevancePropagation
+```
+
+## Manual
+```@contents
+Pages = [
+    "generated/basics.md",
+    "generated/composites.md",
+    "generated/custom_layer.md",
+    "generated/custom_rules.md",
+    "developer.md",
+]
+Depth = 3
+```
+
+## API reference
+```@contents
+Pages = ["api.md"]
+Depth = 2
+```
