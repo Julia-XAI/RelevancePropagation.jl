a80ba770376d03a87ed36d6050a878361d5b704e
Maximilian Ernst <maximilian.stefan.ernst@gmail.com>
2024-03-07 16:20:48 +0100

    add NNlib to weakdeps

--- ext/VisionTransformerExt/VisionTransformerExt.jl
+++ ext/VisionTransformerExt/VisionTransformerExt.jl
@@ -1,5 +1,7 @@
 module VisionTransformerExt
     using RelevancePropagation, Metalhead, Flux
+    using Metalhead: ViPosEmbedding, ClassTokens, _flatten_spatial, MultiHeadSelfAttention
+    using NNlib: split_heads, join_heads
 
     include("layers.jl")
     include("rules.jl")
