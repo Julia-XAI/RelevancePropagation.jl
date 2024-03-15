e7c7bacef46a9085ecb22e812fa66eaeb0041783
Maximilian Ernst <maximilian.stefan.ernst@gmail.com>
2024-03-15 23:48:16 +0100

    formatter

--- ext/VisionTransformerExt/utils.jl
+++ ext/VisionTransformerExt/utils.jl
@@ -2,7 +2,7 @@ function prepare_vit(model::ViT)
     model = model.layers # remove wrapper type
     model = flatten_model(model) # model consists of nested chains
     testmode!(model) # make shure there is no dropout during forward pass
-    if !isa(model[end-2], typeof(seconddimmean))
+    if !isa(model[end - 2], typeof(seconddimmean))
         model = Chain(model[1:(end - 3)]..., SelectClassToken(), model[(end - 1):end]...) # swap anonymous function to actual layer
     end
     return model
