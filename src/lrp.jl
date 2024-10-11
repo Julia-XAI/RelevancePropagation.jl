77d44dbf51bb0c6aa7bc08d27dee924a2bbcfbf4
adrhill <adrian.hill@mailbox.org>
2024-10-11 13:56:11 +0200

    Add new kwarg to docstring

--- src/lrp.jl
+++ src/lrp.jl
@@ -11,8 +11,10 @@ The analyzer can either be created by passing an array of LRP-rules
 or by passing a composite, see [`Composite`](@ref) for an example.
 
 # Keyword arguments
-- `skip_checks::Bool`: Skip checks whether model is compatible with LRP and contains output softmax. Default is `false`.
-- `verbose::Bool`: Select whether the model checks should print a summary on failure. Default is `true`.
+- `normalize_output_relevance`: Selects whether output relevance should be set to 1 before applying LRP backward pass.
+    Defaults to `true` to match literature. If `false`, values of output activations are used.
+- `skip_checks::Bool`: Skip checks whether model is compatible with LRP and contains output softmax. Defaults to `false`.
+- `verbose::Bool`: Select whether the model checks should print a summary on failure. Defaults to `true`.
 
 # References
 [1] G. Montavon et al., Layer-Wise Relevance Propagation: An Overview
